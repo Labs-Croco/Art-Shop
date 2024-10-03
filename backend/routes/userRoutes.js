99096594c63a81f0c963d0186cd42ca35a57584e
maristizabalo <maristizabalo95@gmail.com>
2024-10-03 00:21:25 -0500

    Ending UserController and routes and authMiddleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,33 @@
 import express from "express";
-import { createUSer, loginUser, logoutCurrentUser } from "../controllers/userController.js";
+import { 
+    createUSer, 
+    getAllUsers, 
+    loginUser, 
+    logoutCurrentUser,
+    getCurrentUserProfile,
+    updateCurrentUserProfile,
+    deleteUserById,
+    getUserById,
+    updateUserById 
+} from "../controllers/userController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router()
 
-router.route('/').post(createUSer)
-router.post('/auth', loginUser)
-router.post('/logout', logoutCurrentUser)
+router.route('/').post(createUSer).get(authenticate, authorizeAdmin, getAllUsers);
+router.post('/auth', loginUser);
+router.post('/logout', logoutCurrentUser);
+
+router.route('/profile')
+    .get(authenticate, getCurrentUserProfile)
+    .put(authenticate, updateCurrentUserProfile);
+
+//ADMIN ROUTES
+router.route('/:id')
+.delete(authenticate, authorizeAdmin, deleteUserById)
+.get(authenticate,authorizeAdmin, getUserById)
+.put(authenticate, authorizeAdmin, updateUserById)
+
 
 export default router;
 

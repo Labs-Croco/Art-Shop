43caa06801afec264c898e5c33c10d0cd9aaf417
maristizabalo <84209839+maristizabalo@users.noreply.github.com>
2024-10-20 19:40:55 -0500

    Iniciando pagina de home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,21 +20,21 @@ const Home = () => {
         </Message>
       ) : ( */}
         <>
-          <div className="flex justify-between items-center">
-            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
+          <div className="">
+            <h1 className="">
               Special Products
             </h1>
 
             <Link
               to="/shop"
-              className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
+              className=""
             >
               Shop
             </Link>
           </div>
 
           <div>
-            <div className="flex justify-center flex-wrap mt-[2rem]">
+            <div className="">
               {/* {data.products.map((product) => (
                 <div key={product._id}>
                   <Product product={product} />
